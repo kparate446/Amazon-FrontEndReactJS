ab0ee15599424eab669709fe9b69a6db75c91a33
Krunal <krunalparate96@gmail.com>
2020-10-13 17:48:23 +0530

    Added: Header Component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App () {
+  return (
+    //BEM Conventional in Styling
+    (
+      <div className="App">
+        <Header />
+      </div>
+    )
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB9XQJ9hEl736ssbkEkidkutGOsS-JCbDE",
+  authDomain: "fir-e77b8.firebaseapp.com",
+  databaseURL: "https://fir-e77b8.firebaseio.com",
+  projectId: "fir-e77b8",
+  storageBucket: "fir-e77b8.appspot.com",
+  messagingSenderId: "362054202202",
+  appId: "1:362054202202:web:99a34297dcba103201f963",
+  measurementId: "G-2JMFKZQVNV"
+};
