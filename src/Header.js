c42e6bc348dfdc70db64664ee06911a43b567022
Krunal <krunalparate96@gmail.com>
2020-10-14 15:10:54 +0530

    Add and remove product to the basket

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,20 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import {Link} from 'react-router-dom';
+import {useStateValue} from './StateProvider';
 
 function Header () {
+  const [{basket}, dispatch] = useStateValue ();
+
   return (
     <div className="header">
-      <img
-        className="header_logo"
-        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-      />
+      <Link to="/">
+        <img
+          className="header_logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
       <div className="header_search">
         <input className="header_searchInput" type="text" />
         <SearchIcon className="header_searchIcon" />
@@ -39,12 +45,14 @@ function Header () {
             Prime
           </span>
         </div>
-        <div className="header_optionBasket">
-          <ShoppingCartIcon />
-          <span className="header_optionLineTwo header_basketCount">
-            0
-          </span>
-        </div>
+        <Link to="/checkout">
+          <div className="header_optionBasket">
+            <ShoppingCartIcon />
+            <span className="header_optionLineTwo header_basketCount">
+              {basket.length}
+            </span>
+          </div>
+        </Link>
       </div>
     </div>
   );
