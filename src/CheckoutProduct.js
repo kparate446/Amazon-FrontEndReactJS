8c0437be4031a557aacbec196434f6423960fdc9
Krunal <krunalparate96@gmail.com>
2020-10-26 15:35:10 +0530

    Refactor: Header component

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -18,7 +18,7 @@ function CheckoutProduct({id, image, title, price, rating}) {
       <div className="checkoutProduct_info">
         <p className="checkoutProduct_title">{title}</p>
         <p className="checkoutProduct_price">
-          <small>$</small><strong>{price}</strong>
+          <small>₹</small><strong>{price}</strong>
         </p>
         <div className="checkoutProduct_rating">
           {Array (rating).fill ().map ((_, i) => <p>🌟</p>)}
--- src/Home.js
+++ src/Home.js
@@ -9,14 +9,13 @@ function Home () {
         <img
           className="home_image"
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220.jpg"
-          //   src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Currency_v2_en_US_2x._CB428993290_.jpg"
           alt=""
         />
         <div className="home_row">
           <Product
             id="12365478"
             title="Cadbury Celebrations Assorted Chocolate Gift Pack, 186.6g - Pack of 2"
-            price={2}
+            price={400}
             image="https://images-na.ssl-images-amazon.com/images/I/71nJeIoJE0L._SX679_.jpg"
             rating={5}
           />
@@ -24,7 +23,7 @@ function Home () {
           <Product
             id="12321341"
             title="Apple iPhone 11 (128GB) - Black"
-            price={1000}
+            price={100000}
             image="https://m.media-amazon.com/images/I/51kGDXeFZKL._AC_UY218_.jpg"
             rating={5}
           />
@@ -33,7 +32,7 @@ function Home () {
           <Product
             id="12321341"
             title="The lean startup"
-            price={50}
+            price={500}
             image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
             rating={5}
           />
@@ -41,14 +40,14 @@ function Home () {
           <Product
             id="15265789"
             title="Samsung 6.2 kg Fully-Automatic Top load Washing Machine"
-            price={1000}
+            price={30000}
             image="https://m.media-amazon.com/images/I/81Qqj2C2RzL._AC_UY218_.jpg"
             rating={2}
           />
           <Product
             id="15265789"
             title="Samsung el Core m3, 4GB RAM, 64GB eMMC, 13MP Camera"
-            price={5990}
+            price={60000}
             image="https://m.media-amazon.com/images/I/81xZroil3kL._AC_UY218_.jpg"
             rating={4}
           />
@@ -57,7 +56,7 @@ function Home () {
           <Product
             id="12686446"
             title="Mi TV 4A PRO 80 cm (32 inches) HD Ready Android LED TV"
-            price={1500}
+            price={20000}
             image="https://images-na.ssl-images-amazon.com/images/I/71sBGR6LZhL._SX679_.jpg"
             rating={5}
           />
